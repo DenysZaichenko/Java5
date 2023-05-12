3a6bb771291a4cf98955734db8409aa569e0ec0c
DenysZaichenko <119957396+DenysZaichenko@users.noreply.github.com>
2023-05-12 12:37:32 +0200

    Add files via upload

--- laba5/MainClass.java
+++ laba5/MainClass.java
@@ -4,30 +4,27 @@ import java.io.PrintWriter;
 import java.time.LocalDateTime;
 
 public class MainClass{
-    private static volatile int counter = 0;
     private static final Object lock = new Object();
+
     public static void main(String[] args) {
         try (PrintWriter writer = new PrintWriter(new FileWriter("result.txt"))) {
-            Thread thread1 = new Thread(new WriterTask("Поток 1", 250, writer));
-            Thread thread2 = new Thread(new WriterTask("Поток 2", 500, writer));
-            Thread thread3 = new Thread(new WriterTask("Поток 3", 1000, writer));
+            Thread thread1 = new Thread(new WriterTask("Thread 1", 250, writer));
+            Thread thread2 = new Thread(new WriterTask("Thread 2", 500, writer));
+            Thread thread3 = new Thread(new WriterTask("Thread 3", 1000, writer));
 
             thread1.start();
             thread2.start();
             thread3.start();
 
-            while (counter < 240) {
-                Thread.sleep(100);
-            }
-
-            thread1.interrupt();
-            thread2.interrupt();
-            thread3.interrupt();
+            thread1.join();
+            thread2.join();
+            thread3.join();
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
+
     private static class WriterTask implements Runnable {
 
         private final String name;
@@ -42,8 +39,9 @@ public class MainClass{
 
         @Override
         public void run() {
+            int counter = 0;
             try {
-                while (!Thread.currentThread().isInterrupted()) {
+                while (counter < 240) {
                     LocalDateTime now = LocalDateTime.now();
                     synchronized (lock) {
                         writer.println(name + " - " + now + " - " + counter);
@@ -52,8 +50,9 @@ public class MainClass{
                     Thread.sleep(interval);
                 }
             } catch (InterruptedException e) {
-                
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
         }
-    }   
+    }
 }
